dff10069527efecc8394d431ea6497042bd39f3e
ghnateesh <ghnateesh10@gmail.com>
2020-03-02 00:39:38 +0530

    vote app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1e#y=$lautd6!w_h*go+r4&b(moss%0%a1zg!1vdnto1rd3a%2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
 
